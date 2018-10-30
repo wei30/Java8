864cafadc85027c1bdf7cbd97271da5743fb8b6d
wei30 <weiyili54321@gmail.com>
2018-10-29 21:39:47 -0700

    Update AssertionExample2.java

--- Exception/AssertionExample2.java
+++ Exception/AssertionExample2.java
@@ -2,13 +2,14 @@ package com.exception;
 
 public class AssertionExample2 {
 	enum Car{
-		TWOWHEEL, FOURWHEEL
+		TWOWHEEL, FOURWHEEL, NOWHEEL
 		}
 	public int getDeliveryTimeInDays() {
 		Car status = Car.TWOWHEEL;
 	    switch (status) {
 	        case TWOWHEEL: return 5;
 	        case FOURWHEEL: return 2;
+		case NOWHEEL: return 3;
 	        default:
 	        	assert false: "failed";
 	    }
