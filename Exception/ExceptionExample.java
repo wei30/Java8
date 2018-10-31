fc90f57a226c557ede53e454b9b0ac9827fd5bcc
wei30 <weiyili54321@gmail.com>
2018-10-30 23:57:01 -0700

    Update ExceptionExample.java

--- Exception/ExceptionExample.java
+++ Exception/ExceptionExample.java
@@ -1,24 +1,12 @@
 package com.exception;
 
-public class AssertionExample2 {
-	enum Car{
-		TWOWHEEL, FOURWHEEL
-		}
-	public int getDeliveryTimeInDays() {
-		Car status = Car.TWOWHEEL;
-	    switch (status) {
-	        case TWOWHEEL: return 5;
-	        case FOURWHEEL: return 2;
-	        default:
-	        	assert false: "failed";
-	    }
-	    return 1;
-	}	
+public class ExceptionExample {
+	
+		
 		
 	
   public static void main(String[] args) {
-	AssertionExample2 example = new AssertionExample2();
-	System.out.println(example.getDeliveryTimeInDays());
+	
 
 }
 }
