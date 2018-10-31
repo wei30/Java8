67cb8d1c0871ecc68785f228bc08f1908fe47cfb
wei30 <weiyili54321@gmail.com>
2018-10-30 23:59:08 -0700

    Update ExceptionExample.java

--- Exception/ExceptionExample.java
+++ Exception/ExceptionExample.java
@@ -1,12 +1,16 @@
 package com.exception;
 
 public class ExceptionExample {
-	
+	public void check() throws IOException{
+		System.out.print("throw IOException");
+	}
 		
 		
 	
-  public static void main(String[] args) {
-	
+  public static void main(String[] args) throws IOException{
+	ExceptionExample e = new ExceptionExample();
+	  e.check();
+	  
 
 }
 }
